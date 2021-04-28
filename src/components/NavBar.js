750830793b67042c7ca4fa685898862c837cb34c
kshitij-bhardwaj <74918061+kshitij-bhardwaj@users.noreply.github.com>
2021-04-28 20:49:35 +0530

    Logo added!

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import CustomBtn from './CustomBtn'
-import logo from '../logo.svg'
-import logoMobile from '../logoMobile.svg'
+import logo from '../sernex.svg'
+import logoMobile from '../sernexMobile.svg'
 import {Toolbar, Typography} from '@material-ui/core'
 import {makeStyles} from "@material-ui/core/styles"
 import {BrowserRouter as Router, Link} from 'react-router-dom';
