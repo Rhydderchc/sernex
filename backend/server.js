f777e829ed713aa2048a782b60d56dcba475d4e1
Rhyddy <rhydderchc@gmail.com>
2021-04-29 03:38:23 -0600

    rewrite

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
-const nasa = require('nasa-sdk');
-nasa.setNasaApiKey('WHY?')
-exports.SPACE_DAILY = function(req, res){
-nasa.APOD.fetch().then(data=>{
-    let image = data.hdurl;
-    let description = data.description;
-    res.json({
-        image:image,
-        description:description,
-        credits:"The NASA Development Team",
-        
-    })
-}).catch(err=>{
-    res.status(500).json({
-        error:"Could not fetch new data."
-    })
+const express = require('express');
+const path = require('path');
+const handlers = require('./handlers');
+const app = express();
+let router = express.Router();
+let port = "3000";
+express.application.prefix = express.Router.prefix = function (path, configure) {
+    var router = express.Router();
+    this.use(path, router);
+    configure(router);
+    return router;
+};
+app.prefix('/', function (home) {
+    home.route('/').get(handlers.STATIC_SERVE);
+   
+});
+app.prefix('/space', function(space) {
+    space.route('/').get(handlers.SPACE_INFORMATION)
+    space.route('/dailypicture').get(handlers.SPACE_DAILY)
 })
-}
+app.prefix('/image', function(image){
+    image.route('/').get(handlers.IMAGE_INFORMATION)
 
-exports.STATIC_SERVE = function(req, res){
-    res.sendFile("../public/index.html")
-}
+})
+app.get('/lyrics', handlers.LYRICS_HANDLER)
+app.listen(port);
+console.log('Server started on ' + port);
